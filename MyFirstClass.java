d70f597c8df8aee9863aebace3e66ba9fe6b7e0b
Nathaniel G <naga4744@student.harmonytx.org>
2022-08-19 08:50:43 -0500

    this is my first commit

---
+++ MyFirstClass.java
@@ -0,0 +1,27 @@
+public class MyFirstClass {
+    // after you create your project and class
+    // you have to create your main method to run commands
+    // in order to use shortcuts you type main and press <tab>
+    public static void main(String[] args)
+    {
+        // to print out something to the screen you type System.out.println("your text here");
+        // each line of the code must end with a semicolon ;
+        // shortcut for System.out.println() is sout and <tab>
+        //this double // means this is not part of your code it is just a comment line to explain or taking notes
+
+        // this is single comment line but if you need multiple lines
+
+        /*
+
+            this will go on until the
+            end
+            of
+            the comment
+         */
+        System.out.println("testing");
+
+        // another way to print on screen with System.out.print();
+        // the difference is that with multiple prints it will not format it on the next line and just put all of them on
+        // the same line of text
+    }
+}
