2632fdbc61d5d6267b37ae89c7dc74a4be79d15a
Nathaniel G <naga4744@student.harmonytx.org>
2022-08-19 09:10:54 -0500

    this is my second commit

--- variables.java
+++ variables.java
@@ -1,6 +1,27 @@
 public class variables
 {
     public static void main(String[] args) {
-        String name = "Nathaniel";
+        //DECLARATION OF VARIABLES
+        //  Datatype VariableName = value;
+            String name = "Nathaniel";   // Text
+            int myNum = 1738;            // Integer (whole number)
+            float myFloatNum = 9.99f;    // Floating point number
+            char myLetter = 'N';         // Character
+            boolean myBool = true;       // Boolean
+            byte smallNumber = 2;        // Stores whole numbers from -128 to 127
+            short shortNumber = 4;       // Stores whole numbers from -32,768 to 32,767
+            long longNumber = 123456789; // Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+            double doubleNumber = 12.45; // Stores fractional numbers. Sufficient for storing 15 decimal digits
+            // RULES FOR VARIABLES AND DATA TYPES
+
+            // You can not give reserved names for a variable like class etc...
+            // It should not start from a number
+            // Start always with lowercase, you can start with a uppercase but it is not preferred
+            // primitives are byte, short, long, int, float, double, char and boolean
+            // Float ends with f
+            // Double ends with d, but it is not required to work
+            // Char has only one character within single quote
+            // Long ends with l, but it is not required to work
+            // Boolean only has true or false as value
     }
 }
