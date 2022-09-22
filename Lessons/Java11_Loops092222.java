483dd79d11f83fc7467ce0c4abc5a2df1a1eefdd
Nathaniel G <naga4744@student.harmonytx.org>
2022-09-22 09:39:59 -0500

    Today is 9/22/2022

---
+++ Lessons/Java11_Loops092222.java
@@ -0,0 +1,105 @@
+package Lessons;
+
+public class Java11_Loops092222
+{
+    public static void main(String[] args)
+    {
+
+        // LOOPS
+        // allow you to execute commands as many times as you need.
+        // There are three types of loops, for, while, and do while
+
+        // FOR LOOP
+
+        for(int i =0 ; i<=10 ; i++ ) // this is going to increase number 1 by 1 from 0 to 10
+       {
+          //  System.out.println(i);
+        }
+
+        for(int i = 2; i<6 ; i+=2 ) // increases 3 by 3 starts from 2 and ends at 5
+        {
+          //  System.out.println(i);
+        }
+
+        for(int i = 1 ; i<=100 ; i++  ) // this will print hello word 100 times
+        {
+          //  System.out.println("Hello World");
+        }
+
+        for(int i=10;i>=0;i--) // this counts down from 10 to 0 by 1
+        {
+          //  System.out.println(i);
+        }
+
+
+
+        for(int i =0; i<= 1000; i+=2)
+        {
+        //    System.out.println(i);
+        }
+
+        for(int i=10;i<=40;i+=2)
+        {
+        //    System.out.println(i);
+        }
+
+        for(int i=0;i<=100;i+=5)
+        {
+        //    System.out.println(i);
+        }
+
+      //  for(int i=3;i<=100;i++)
+     //   {
+      //      if(i%3==0||i%5==0)
+     //       {
+      //          System.out.println(i);
+      //     }
+
+     //   }
+
+
+        // NESTED LOOP
+        // is a loop within another loop
+
+      //  for(int i = 0;i<4;i++)
+      //  {
+       //     for(int j=0;j<4;j++)
+       //     {
+       //         System.out.println("When i="+i+" j="+j);
+       //     }
+      //  }
+
+
+
+        //     for(int i =0,j=6;i<100&&j<100;i++,j++)
+        //      {
+        //         System.out.println("i= "+i+", j="+j);
+        //    }
+
+
+        // INFINITY LOOP
+        // if you want something to run until forever
+     //   for(int b=0; 3<5; b++)
+    //    {
+    //        System.out.println("Hello world!"+ " iteration number "+b);
+    //    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ apcsaProjects2233/projectNumberTable092222.java
@@ -0,0 +1,12 @@
+package apcsaProjects2233;
+
+public class projectNumberTable092222
+{
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i+" "+i*i);
+        }
+    }
+}
